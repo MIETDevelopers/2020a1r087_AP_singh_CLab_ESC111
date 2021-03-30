c4dd37d47ca68e615707a8b58fc271b7f9483b95
unknown <apsingh9922@gmail.com>
2021-03-30 13:12:23 +0530

    addition of two numbers

---
+++ lab02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{	int x, y, z;
+	printf("enter the value of x :\n");
+	scanf("%d" , &x);
+	printf("enter the value of y :\n");
+	scanf("%d" , &y);
+	z = x + y;
+	printf("z = %d\n", z);
+
+	return 0;
+}
